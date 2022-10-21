e97225e1aabc81d1c8e16f8fd67d31be6c2aae4f
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-10-21 10:03:33 -0500

    Clase 21-10

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from database import db
+from flask_migrate import Migrate
+from models import Persona
+
+
+app = Flask(__name__)
+
+#Configuración de la bD
+USER_DB = "postgres"
+PASS_DB = "admin"
+URL_DB = "localhost"
+NAME_DB = "flask_db"
+FULL_URL_DB = f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = FULL_URL_DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+#Configurar migración
+migrate = Migrate()
+migrate.init_app(app,db)
+
+
