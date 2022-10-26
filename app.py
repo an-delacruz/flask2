1eeeba1ae8f8c1e294a945350a4efdefb48d9756
Antonio De la cruz <antoniocruzrivera0216@gmail.com>
2022-10-26 09:43:12 -0500

    Crud finalizado

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,request,url_for,render_template,redirect
 from database import db
 from flask_migrate import Migrate
 from models import Persona
+from forms import PersonaForm
 
 app = Flask(__name__)
 
@@ -21,4 +22,50 @@ db.init_app(app)
 migrate = Migrate()
 migrate.init_app(app,db)
 
+#Form
+app.config["SECRET_KEY"] = "123"
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index.html')
+def inicio():
+    personas = Persona.query.all()
+    return render_template('index.html',personas=personas)
+
+@app.route("/ver/<int:id>")
+def verDetalle(id):
+    persona = Persona.query.get_or_404(id)
+    return render_template('detalle.html',persona=persona)
+
+@app.route("/agregar",methods=['GET','POST'])
+def agregar():
+    persona = Persona()
+    personaForm = PersonaForm(obj=persona)
+    if request.method == 'POST':
+        if personaForm.validate_on_submit():
+            personaForm.populate_obj(persona)
+            #insert
+            db.session.add(persona)
+            db.session.commit()
+            return redirect(url_for('inicio'))
+    return render_template('agregar.html',forma=personaForm)
+
+@app.route("/editar/<int:id>",methods=['GET','POST'])
+def editar(id):
+    persona = Persona.query.get_or_404(id)
+    personaForm = PersonaForm(obj=persona)
+    if request.method == 'POST':
+        if personaForm.validate_on_submit():
+            personaForm.populate_obj(persona)
+            #update
+            db.session.commit()
+            return redirect(url_for('inicio'))
+    return render_template('editar.html',forma=personaForm)
+
+@app.route("/eliminar/<int:id>")
+def eliminar(id):
+    persona = Persona.query.get_or_404(id)
+    db.session.delete(persona)
+    db.session.commit()
+    return redirect(url_for('inicio'))
 
---
+++ forms.py
@@ -0,0 +1,11 @@
+from ast import Sub
+from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+class PersonaForm(FlaskForm):
+    nombre = StringField('Nombre',validators=[DataRequired()])
+    apellido = StringField('Apellido',)
+    email = StringField('Email',validators=[DataRequired()])
+    enviar = SubmitField('Enviar')
